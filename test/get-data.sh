6c51e4898558280e3ca70e1ac8f01ed0d3ec8f6f
Jie Kang <jkang@redhat.com>
2024-03-21 17:26:01 -0400

    Update get-data to acquire release data

--- test/get-data.sh
+++ test/get-data.sh
@@ -9,13 +9,26 @@ mkdir -p data
 PIPELINE_NAME=release-openjdk${VERSION}-pipeline
 PIPELINE_INFO=data/${PIPELINE_NAME}.json
 
-echo "Processing ${PIPELINE_NAME}"
+echo "Processing releases for JDK ${VERSION}"
 
-URL="https://trss.adoptium.net/api/getBuildHistory?buildName=${PIPELINE_NAME}&url=https://ci.adoptium.net/job/build-scripts&limit=120"
+RELEASE_INFO=data/releases${VERSION}.json
 
-curl -s -X 'GET' \
-    ${URL} \
-    -H 'accept: application/json' >${PIPELINE_INFO}
+RELEASES_URL="https://api.adoptium.net/v3/assets/feature_releases/${VERSION}/ga?heap_size=normal&image_type=jdk&jvm_impl=hotspot&page=0&page_size=10&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+
+if [ ! -f ./${RELEASES_INFP} ]; then
+    curl -s -X 'GET' \
+        ${RELEASES_URL} \
+        -H 'accept: application/json' >${RELEASE_INFO}
+fi
+echo "Processing pipelines for JDK ${VERSION}"
+
+PIPELINE_URL="https://trss.adoptium.net/api/getBuildHistory?buildName=${PIPELINE_NAME}&url=https://ci.adoptium.net/job/build-scripts&limit=120"
+
+if [ ! -f ./${PIPELINE_INFO} ]; then
+    curl -s -X 'GET' \
+        ${PIPELINE_URL} \
+        -H 'accept: application/json' >${PIPELINE_INFO}
+fi
 
 DATA=$(jq '.[] | select(.status == "Done") | ._id' ${PIPELINE_INFO})
 mkdir -p data/totals/
@@ -26,3 +39,6 @@ for ID in $(echo $DATA | tr -d \"); do
         ${TOTALS_URL} \
         -H 'accept: application/json' >data/totals/${ID}.json
 done
+
+LEVELS="dev sanity extended special"
+GROUPS="build functional openjdk system external perf jck"
