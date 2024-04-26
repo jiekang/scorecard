1b593b8b71f689a3042617a4feed03fe25504427
Jie Kang <jkang@redhat.com>
2024-04-26 15:15:42 -0400

    Update get-data to acquire jdk and test child job data

--- test/get-data.sh
+++ test/get-data.sh
@@ -1,8 +1,18 @@
+# e.g. 17
 VERSION=$1
 if [ -z $VERSION ]; then
     echo "Usage $0 version-number"
     exit
 fi
+
+# e.g. 2024-04-01
+DATE=$2
+if [ -z $DATE] ]; then
+    echo "Usage $0 version-number date"
+    exit
+fi
+
+TIMESTAMP=`date -u -d "${DATE}" "+%s%3N"`
 
 mkdir -p data
 
@@ -15,14 +25,14 @@ RELEASE_INFO=data/releases${VERSION}.json
 
 RELEASES_URL="https://api.adoptium.net/v3/assets/feature_releases/${VERSION}/ga?heap_size=normal&image_type=jdk&jvm_impl=hotspot&page=0&page_size=10&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
 
-if [ ! -f ./${RELEASES_INFP} ]; then
+if [ ! -f ./${RELEASES_INFO} ]; then
     curl -s -X 'GET' \
         ${RELEASES_URL} \
         -H 'accept: application/json' >${RELEASE_INFO}
 fi
 echo "Processing pipelines for JDK ${VERSION}"
 
-PIPELINE_URL="https://trss.adoptium.net/api/getBuildHistory?buildName=${PIPELINE_NAME}&url=https://ci.adoptium.net/job/build-scripts&limit=120"
+PIPELINE_URL="https://trss.adoptium.net/api/getBuildHistory?buildName=${PIPELINE_NAME}&status=Done&url=https://ci.adoptium.net/job/build-scripts&limit=120"
 
 if [ ! -f ./${PIPELINE_INFO} ]; then
     curl -s -X 'GET' \
@@ -30,14 +40,30 @@ if [ ! -f ./${PIPELINE_INFO} ]; then
         -H 'accept: application/json' >${PIPELINE_INFO}
 fi
 
-DATA=$(jq '.[] | select(.status == "Done") | ._id' ${PIPELINE_INFO})
+BUILD_IDS=$(jq '.[] | select(.timestamp > '${TIMESTAMP}') | ._id' ${PIPELINE_INFO})
 mkdir -p data/totals/
+mkdir -p data/child/
 
-for ID in $(echo $DATA | tr -d \"); do
-    TOTALS_URL=https://trss.adoptium.net/api/getTotals?id=${ID}
+echo "Processing builds for JDK ${VERSION}"
+
+for ID in $(echo ${BUILD_IDS} | tr -d \"); do
+    echo "Acquiring totals for ${ID}"
+    TOTALS_URL="https://trss.adoptium.net/api/getTotals?id=${ID}"
     curl -s -X 'GET' \
         ${TOTALS_URL} \
         -H 'accept: application/json' >data/totals/${ID}.json
+
+    echo "Acquiring jdk jobs for ${ID}"
+    CHILD_JDK_URL="https://trss.adoptium.net/api/getAllChildBuilds?buildNameRegex=%5E(jdk%5B0-9%5D%7B1%2C2%7D%7CBuild_)&parentId=${ID}"
+    curl -s -X 'GET' \
+        ${CHILD_JDK_URL} \
+        -H 'accept: application/json' >data/child/jdk-${ID}.json
+
+    echo "Acquiring test jobs for ${ID}"
+    CHILD_TEST_URL="https://trss.adoptium.net/api/getAllChildBuilds?buildNameRegex=%5ETest_openjdk.*&parentId=${ID}"
+    curl -s -X 'GET' \
+        ${CHILD_TEST_URL} \
+        -H 'accept: application/json' >data/child/test-${ID}.json
 done
 
 LEVELS="dev sanity extended special"
