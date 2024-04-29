7a8b226c47b66746eb0f00dc56ceefec8022eee5
Jie Kang <jkang@redhat.com>
2024-04-29 15:51:22 -0400

    Update compute with version value for test data

--- test/compute.sh
+++ test/compute.sh
@@ -4,8 +4,6 @@ LEVELS="dev sanity extended special"
 GROUPS="build functional openjdk system external perf jck"
 LINUX_SYSTEMS="x86-64_linux aarch64_linux s390x_linux ppc64le_linux"
 
-
-
 function computeBuild() {
     ID=$1
 
@@ -15,10 +13,15 @@ function computeTest() {
     ID=$1
     OS=$2
     FILE=data/child/test-${ID}.json
-    RESULT=$(jq '[.[] | {"buildName": .buildName, "buildDuration": .buildDuration, "testSummary": .testSummary, "platform": .buildParams | .[] | select(.name=="PLATFORM") | .value}]' ${FILE})
-    echo ${RESULT} > data/child/test-${ID}-compute.json
+    RESULT=$(jq '[.[] | 
+        {"buildName": .buildName,
+        "buildDuration": .buildDuration,
+        "testSummary": .testSummary,
+        "platform": .buildParams | .[] | select(.name=="PLATFORM") | .value,
+        "version": .buildParams | .[] | select(.name=="JDK_VERSION") | .value
+        }]' ${FILE})
+    echo ${RESULT} >data/child/test-${ID}-compute.json
 }
-
 
 TOTAL_EXECUTION_TIME=0
 
--- test/tester.sh
+++ test/tester.sh
@@ -2,9 +2,12 @@ ID=661f70c9879917006e985908
 
 FILE=data/child/test-${ID}.json
 
-RESULT=$(jq '[.[] | {"buildName": .buildName, "buildDuration": .buildDuration, "testSummary": .testSummary, "platform": .buildParams | .[] | select(.name=="PLATFORM") | .value}]' ${FILE})
-
-# RESULT=$(jq '.[] | .buildParams | .[] | select(.name=="PLATFORM")' ${FILE})
-
+RESULT=$(jq '[.[] | 
+    {"buildName": .buildName,
+    "buildDuration": .buildDuration,
+    "testSummary": .testSummary,
+    "platform": .buildParams | .[] | select(.name=="PLATFORM") | .value,
+    "version": .buildParams | .[] | select(.name=="JDK_VERSION") | .value
+    }]' ${FILE})
 echo $RESULT
-echo $RESULT > tmp.json
+echo $RESULT >tmp.json
