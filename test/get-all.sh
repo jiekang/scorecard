b3c9b730edd6ab935128ce31e37bf358d6c37e49
Jie Kang <jkang@redhat.com>
2024-07-04 08:40:50 -0400

    Update get-all with date range support

--- test/get-all.sh
+++ test/get-all.sh
@@ -1,9 +1,15 @@
-DATE=$1
-if [ -z ${DATE} ]; then
-    echo "Usage $0 date"
+AFTER_DATE=$1
+if [ -z ${AFTER_DATE} ]; then
+    echo "Usage $0 after-date before-date"
+    exit
+fi
+
+BEFORE_DATE=$2
+if [ -z ${BEFORE_DATE} ]; then
+    echo "Usage $0 after-date before-date"
     exit
 fi
 
 for VERSION in 8 11 17 21; do
-    ./get-data.sh ${VERSION} $DATE
+    ./get-data.sh ${VERSION} ${AFTER_DATE} ${BEFORE_DATE}
 done
